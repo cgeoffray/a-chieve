950fac30068b1a1b5147b95f5c20ebecceecf7f7
Corentin Geoffray <corentin@wattease.com>
2013-10-28 22:11:39 -0700

    db changes for categories

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,5 @@
 class Job < ActiveRecord::Base
 	has_many :videos, :dependent => :delete_all
+	has_many :jobs, :as => :category
+	belongs_to :category, :class_name => "Job"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131011002504) do
+ActiveRecord::Schema.define(version: 20131029050153) do
 
   create_table "jobs", force: true do |t|
     t.string   "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20131011002504) do
     t.text     "education"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
+    t.integer  "level"
   end
 
   create_table "users", force: true do |t|
