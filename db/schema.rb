6250339ad7cd30da7203856077d7cc7f5fc129bc
Greg <gbonnat@gmail.com>
2013-11-08 07:45:24 +0100

    minor frontend

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131031233309) do
-
-  create_table "categories", force: true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "video_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20131029075331) do
 
   create_table "jobs", force: true do |t|
     t.string   "title"
@@ -72,9 +64,6 @@ ActiveRecord::Schema.define(version: 20131031233309) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role_id",                default: 1
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
