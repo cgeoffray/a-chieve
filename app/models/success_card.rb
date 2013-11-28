2ea726acc4668ab79c1ac88823809b75b8c11c2b
Samuel Peter <2010.samuel.peter@gmail.com>
2013-11-28 15:24:23 -0800

    Fix paperclip s3 connection bug

--- app/models/success_card.rb
+++ app/models/success_card.rb
@@ -1,7 +1,7 @@
 class SuccessCard < ActiveRecord::Base
   belongs_to :job
 
-  has_attached_file :picture, styles: {
+  has_attached_file :picture, path: "/:class/:attachment/:id_partition/:style/:filename'",  url: ":s3_working_url", styles: {
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
